59294d437545a630dda9587ac7baac372164fa50
LAPTOP-I0PBB5PG <pankaj.sinha@yahoo.co.in>
2022-01-10 17:33:18 +0530

    Google Analytics Added

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { Layout } from '../components';
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.scss';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Script 
+  strategy='lazyOnload'
+  src={`https://www.googletagmanager.com/gtag/js?id=G-ESH7246P8V`}
+/>
+  <Script 
+  strategy='lazyOnload'>
+    {
+      `
+     window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-ESH7246P8V');
+    `
+    }
+  </Script>
       <Component {...pageProps} />
     </Layout>
   )
